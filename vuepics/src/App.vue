378aa385a4f64698393f2c3fd5ebff9b983c0b88
emanuelecomp <cardgameheritage@gmail.com>
2018-04-02 22:56:49 -0300

    vue is funny and ez

---
+++ vuepics/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+        <li v-for="foto of fotos">
+            <img :src="foto.url" :alt="foto.titulo">
+        </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'VuePics',
+      fotos:[
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorrinho'
+        }
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
